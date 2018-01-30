226702b1f32a6e90a8594fb86e79d0df2a758e54
Nikita Mikhailov <nikita.s.mikhailov@gmail.com>
2018-01-30 22:49:35 +0700

    Fix style

--- si2cce.c
+++ si2cce.c
@@ -9,43 +9,44 @@
 
 pid_t child_pid;
 
-void send_ctrl_event(int s){
-	GenerateConsoleCtrlEvent(0, child_pid);
+void send_ctrl_event(int s) {
+    GenerateConsoleCtrlEvent(0, child_pid);
 }
 
 int main(int argc, char* argv[]) {
-	struct sigaction sigIntHandler;
-	sigset_t set, oset;
+    struct sigaction sigIntHandler;
+    sigset_t set, oset;
 
-	if (argc < 2) {
-		printf("Usage: %s <program name> [program args]\n", argv[0]);
-		return -1;
-	}
+    if (argc < 2) {
+        printf("Usage: %s <program name> [program args]\n", argv[0]);
+        return -1;
+    }
 
-	// Block SIGINT
-	sigemptyset(&set);
-	sigaddset(&set, SIGINT);
-	sigprocmask(SIG_BLOCK, &set, &oset);
+    // Block SIGINT
+    sigemptyset(&set);
+    sigaddset(&set, SIGINT);
+    sigprocmask(SIG_BLOCK, &set, &oset);
 
-	child_pid = fork();	
-	if (child_pid == 0) {
-	    execvp(argv[1], argv + 1);
-	    return 0;
-	}
+    child_pid = fork();
+    if (child_pid == 0) {
+        execvp(argv[1], argv + 1);
+        return 0;
+    }
 
-	// Unblock SIGINT
-	sigprocmask(SIG_SETMASK, &oset, NULL);
+    // Unblock SIGINT
+    sigprocmask(SIG_SETMASK, &oset, NULL);
 
-	// Install handler
-	sigIntHandler.sa_handler = send_ctrl_event;
-	sigemptyset(&sigIntHandler.sa_mask);
-	sigIntHandler.sa_flags = 0;
-	sigaction(SIGINT, &sigIntHandler, NULL);
+    // Install handler
+    sigIntHandler.sa_handler = send_ctrl_event;
+    sigemptyset(&sigIntHandler.sa_mask);
+    sigIntHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigIntHandler, NULL);
 
-	// Wait for finish 
-	while(waitpid(child_pid, NULL, WNOHANG) == 0) {
-		sleep(1);
-	}
-	
-	return 0;
+    // Wait for finish
+    while(waitpid(child_pid, NULL, WNOHANG) == 0) {
+        sleep(1);
+    }
+
+    return 0;
 }
+
